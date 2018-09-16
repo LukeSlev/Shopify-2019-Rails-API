b73844186c6abfe347b75294985b28282b858d65
LukeSlev <lslevins@ualberta.ca>
2018-09-16 16:18:55 -0600

    fix some dumb autogenerated specs

--- spec/routing/line_items_routing_spec.rb
+++ spec/routing/line_items_routing_spec.rb
@@ -1,30 +1,30 @@
 require "rails_helper"
 
-RSpec.describe LineItemsController, type: :routing do
+RSpec.describe Api::V1::LineItemsController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(:get => "/line_items").to route_to("line_items#index")
+      expect(:get => "/api/v1/shops/1/orders/1/line_items").to route_to("api/v1/line_items#index", :shop_id => "1", :order_id => "1")
     end
 
     it "routes to #show" do
-      expect(:get => "/line_items/1").to route_to("line_items#show", :id => "1")
+      expect(:get => "/api/v1/shops/1/orders/1/line_items/1").to route_to("api/v1/line_items#show", :id => "1", :shop_id => "1", :order_id => "1")
     end
 
 
     it "routes to #create" do
-      expect(:post => "/line_items").to route_to("line_items#create")
+      expect(:post => "/api/v1/shops/1/orders/1/line_items").to route_to("api/v1/line_items#create", :shop_id => "1", :order_id => "1")
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/line_items/1").to route_to("line_items#update", :id => "1")
+      expect(:put => "/api/v1/shops/1/orders/1/line_items/1").to route_to("api/v1/line_items#update", :id => "1", :shop_id => "1", :order_id => "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/line_items/1").to route_to("line_items#update", :id => "1")
+      expect(:patch => "/api/v1/shops/1/orders/1/line_items/1").to route_to("api/v1/line_items#update", :id => "1", :shop_id => "1", :order_id => "1")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/line_items/1").to route_to("line_items#destroy", :id => "1")
+      expect(:delete => "/api/v1/shops/1/orders/1/line_items/1").to route_to("api/v1/line_items#destroy", :id => "1", :shop_id => "1", :order_id => "1")
     end
   end
 end
